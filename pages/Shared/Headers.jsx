414b9317aae94afdc6898dbac11b05d9a108f0ac
nca49 <vladshining@gmail.com>
2021-01-19 08:13:56 +0300

    debug src image

--- pages/Shared/Headers.jsx
+++ pages/Shared/Headers.jsx
@@ -1,15 +1,14 @@
 import headers from  "../../styles/headers.module.scss";
-import profil from "../../public/img/profil.png";
 import React from "react";
+import Image from "next/image";
 
 export default function Headers(){
     return (
         <div className={headers.header}>
-            <img src={profil} alt=""/>
+               <Image className ={headers.image} src="/img/profil.png" alt="" width={300} height={300}/>
             <p>
                 I'm <span>493.649</span> Developer front , designer.
             </p>
-
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, corporis! Labore error sequi temporibus velit possimus incidunt quidem dolore omnis ducimus! Facere minus quos nulla! Eaque distinctio id minima nobis?
         </div>
     )
